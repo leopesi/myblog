f249bb9418fe6a8c423dab00a8f3bb440261764f
LEONARDO PEREIRA DA SILVA <52714788+leopesi@users.noreply.github.com>
2022-03-28 11:13:35 -0300

    Update forms.py

--- post/forms.py
+++ post/forms.py
@@ -6,11 +6,6 @@ class PostForm(ModelForm):
     title = CharField(
         label='Título',
     )
-    author = CharField(
-        label='Autor',
-    )
-
-
 
     slug = CharField(
         label='Slug',
