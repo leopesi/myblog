507b4dee0053aa1fceda1152b464fba11b78a4dc
Leonardo <leopesi@yahoo.com.br>
2022-04-01 11:47:26 -0300

    setup settings

--- myblog/settings.py
+++ myblog/settings.py
@@ -25,10 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7^qto!9*@l+z87iq+&nre=ddva964f&0+hlz&d!$xit6&r594q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [ ]
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
